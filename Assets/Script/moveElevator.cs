16413e6353d07b048186706c4845076236cac98e
Diegodm35 <alu0101337975@ull.edu.es>
2023-02-03 20:24:13 +0000

    Elevator sube

--- Assets/Script/moveElevator.cs
+++ Assets/Script/moveElevator.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class moveElevator : MonoBehaviour
 {
+    bool isOnElevator = false;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.parent = this.transform;
+            isOnElevator = true;
+            // Quitar el parent
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,12 @@ public class moveElevator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isOnElevator)
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                new Vector3(transform.position.x, transform.position.y + 5, transform.position.z),
+                Time.deltaTime);
+        }
     }
 }
