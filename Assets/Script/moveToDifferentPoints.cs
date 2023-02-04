2348d2608147455c1c07007c460e1cf81749475e
Diegodm35 <alu0101337975@ull.edu.es>
2023-02-04 10:44:40 +0000

    Movimiento de plataformas finalizado

--- Assets/Script/moveToDifferentPoints.cs
+++ Assets/Script/moveToDifferentPoints.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class moveToDifferentPoints : MonoBehaviour
 {
+    public float speed;
+    public Transform[] points;
+    public int currentPoint;
+
+    private int index;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = points[currentPoint].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(transform.position, points[index].position) < 0.02f)
+        {
+            index++;
+            if (index == points.Length) {
+                index = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[index].position, speed * Time.deltaTime);
     }
 }
