f5c22e7346b695cfef4a5e6a288fbe4e6cceb1b3
Diegodm35 <alu0101337975@ull.edu.es>
2023-02-04 14:22:40 +0000

    Refactor de moveWhenIsOn

--- Assets/Script/moveWhenPlayerOn.cs
+++ Assets/Script/moveWhenPlayerOn.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class moveWhenPlayerOn : MonoBehaviour
 {
-    [Tooltip("Determines in wich axis the platform will move")]
-    [SerializeField] private bool moveOnX = false;
+    [Tooltip("Velocity of the object")]
+    [SerializeField] public float speed;
 
-    [Tooltip("The max position the platform can reach")]
-    [SerializeField] private Transform finalPositionTransform;
+    [Tooltip("GameObject to move to")]
+    [SerializeField] public Transform[] points;
 
     private Transform playerTransform;
     bool isOnElevator = false;
 
+    private int index = 1;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -43,55 +44,17 @@ public class moveWhenPlayerOn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (isOnElevator)
-        //{
-        //    float playerPosition, finalPosition;
-        //    if(moveOnX)
-        //    {
-        //        playerPosition = transform.position.x;
-        //        finalPosition = finalPositionTransform.position.x;
-        //    }
-        //    else
-        //    {
-        //        playerPosition = transform.position.y;
-        //        finalPosition = finalPositionTransform.position.y;
-        //    }
-        //    //cambiar a plataforma movil ciclica
-        //    if (playerPosition > finalPosition)
-        //    {
-        //        playerTransform.parent = null;
-        //        isOnElevator = false;
-        //        return;
-        //    }
-        //}
         if (isOnElevator)
         {
-            if (moveOnX)
+            if (Vector2.Distance(transform.position, points[index].position) < 0.02f)
             {
-                if (transform.position.x > finalPositionTransform.position.x)
+                index++;
+                if (index == points.Length)
                 {
-                    playerTransform.parent = null;
-                    isOnElevator = false;
-                    return;
+                    index = 0;
                 }
-                transform.position = Vector3.Lerp(
-                    transform.position,
-                    new Vector3(transform.position.x + 5, transform.position.y, transform.position.z),
-                    Time.deltaTime);
             }
-            else
-            {
-                if (transform.position.y > finalPositionTransform.position.y)
-                {
-                    playerTransform.parent = null;
-                    isOnElevator = false;
-                    return;
-                }
-                transform.position = Vector3.Lerp(
-                    transform.position,
-                    new Vector3(transform.position.x, transform.position.y + 5, transform.position.z),
-                    Time.deltaTime);
-            }
+            transform.position = Vector2.MoveTowards(transform.position, points[index].position, speed * Time.deltaTime);
         }
     }
 }
