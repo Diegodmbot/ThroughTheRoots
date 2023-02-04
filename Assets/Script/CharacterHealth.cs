ad44505a5f46bfcc469d875847ad00aa0533378e
Diegodm35 <alu0101337975@ull.edu.es>
2023-02-04 15:18:41 +0000

    Corazones vinculados al script

--- Assets/Script/CharacterHealth.cs
+++ Assets/Script/CharacterHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterHealth : Health {
   [Tooltip("The UI images that represent the health points")]
